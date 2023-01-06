6b24804ff0a1e537edcdfaeb556470aeebb914fb
Theophileaseh <theophilenwachan@gmail.com>
2023-01-06 12:11:14 +0100

    set POST and GET conditions

--- doctors/views.py
+++ doctors/views.py
@@ -1,13 +1,19 @@
 from django.http import JsonResponse
 from .models import Doctor
 from .serializers import DoctorSerializer
+from rest_framework.decorators import api_view
 
+@api_view('GET', 'POST')
 def doctor_list(request):
 
   #get all doctors
   #serializer them
   #return json
 
-  doctors = Doctor.objects.all()
-  serializer = DoctorSerializer(doctors, many=True)
-  return JsonResponse(serializer.data, safe=False)
+  if request.method == 'GET':
+    doctors = Doctor.objects.all()
+    serializer = DoctorSerializer(doctors, many=True)
+    return JsonResponse(serializer.data, safe=False)
+
+  if request.method == 'POST':
+    serializer = DoctorSerializer(data = request.data)
