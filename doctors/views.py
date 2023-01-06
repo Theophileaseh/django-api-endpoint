fd70cc05ce995f563c5633913b68a6f9e588e49b
Theophileaseh <theophilenwachan@gmail.com>
2023-01-06 13:12:22 +0100

    Add delete method for doctor

--- doctors/views.py
+++ doctors/views.py
@@ -34,12 +34,15 @@ def doctor_detail(request, id):
   if request.method == 'GET':
     serializer = DoctorSerializer(doctor)
     return Response(serializer.data)
+
   elif request.method == 'PUT':
     serializer = DoctorSerializer(doctor, data = request.data)
     if serializer.is_valid():
       serializer.save()
       return Response(serializer.data)
-    return Response(serializer.error, status = status.HTTP_400_BAD_REQUEST)
+    return Response(serializer.errors, status = status.HTTP_400_BAD_REQUEST)
+
   elif request.method == 'DELETE':
-    pass
+    doctor.delete()
+    return Response(status = status.HTTP_204_NO_CONTENT)
 
