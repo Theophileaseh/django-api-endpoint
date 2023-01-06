d6888ecabf7b13ef74b0954e0c13a720e5cc3163
Theophileaseh <theophilenwachan@gmail.com>
2023-01-06 09:43:01 +0100

    Create model for doctors and migration

---
+++ doctors/models.py
@@ -0,0 +1,7 @@
+from django.db import models
+
+class Doctor(models.Model):
+  name = models.CharField(max_length=200)
+  image = models.CharField(max_length=300)
+  specialty = models.CharField(max_length=200)
+  bio = models.CharField(max_length=500)
